391128399eb0c1bca7b1892c4d434527bec41fc2
hasanaks <hasanarda2015@gmail.com>
2022-06-29 21:40:08 +0300

    add support for running files

--- toy.c
+++ toy.c
@@ -3,13 +3,53 @@
 #include "compiler.h"
 #include "vm.h"
 
-int main(void) {
-  struct VM vm;
-  initVM(&vm);
+static void runRepl(void) {
+	// TODO: implement a repl
+}
 
-  struct Chunk chunk = compileString(".25");
-  runVM(&vm, &chunk);
-  deinitChunk(&chunk);
+static char* readFile(const char* fileName) {
+	FILE* file = fopen(fileName, "r");
+	if (file == NULL) {
+		printf("could not read %s\n", fileName);
+		return NULL;
+	}
 
-  deinitVM(&vm);
+	fseek(file, 0, SEEK_END);
+	size_t fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	char* contents = malloc(fileSize + 1); // + 1 for '\0'
+	fread(contents, 1, fileSize, file);
+	contents[fileSize] = '\0';
+
+	fclose(file);
+
+	return contents;
+}
+
+static void runFile(const char* fileName) {
+	char* source = readFile(fileName);
+	if (source == NULL) {
+		exit(1);
+	}
+	
+	struct Chunk compiled = compileString(source);
+
+	struct VM vm;
+	initVM(&vm);
+	runVM(&vm, &compiled);
+	deinitVM(&vm);
+
+	deinitChunk(&compiled);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+		runRepl();
+	} else if (argc == 2) {
+		char* fileName = argv[1];
+		runFile(fileName);
+	} else {
+		puts("usage: toy [FILE]");
+	}
 }
